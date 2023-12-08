1ebb2985f79aa056fa548b251d68b419178aa74b
Nicolas <nico.daddabbo7100@gmail.com>
2023-12-08 10:02:26 +0100

    mise en place GetAllUsers

---
+++ User_API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using User_API.Models;
+
+namespace User_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+
+        private readonly List<User> _fakeDbTableUser = FakeDb.Users;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAllUser()
+        {
+            IEnumerable<User> users = _fakeDbTableUser;
+            if (users != null)
+                return Ok(users);
+
+            return NotFound();
+                
+            
+        }
+    }
+   
+}
