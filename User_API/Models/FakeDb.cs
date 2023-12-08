2481976081c6232200bccd8e50ee6b68a6a9a017
Nicolas <nico.daddabbo7100@gmail.com>
2023-12-08 10:18:11 +0100

    merge de getAll + modif sur la classe User

--- User_API/Models/FakeDb.cs
+++ User_API/Models/FakeDb.cs
@@ -4,21 +4,26 @@ namespace User_API.Models
 {
     public static class FakeDb 
     {
-
+        private static int id = 1;
         public static List<User> Users = new List<User>()
         {
-            new User("Nicolas", "D'Addabbo", "nico.d@gmail.com", "Test123123="),
-            new User("Jean", "Dupont", "jean.dupont@gmail.com", "MotDePasse123="),
-            new User("Marie", "Martin", "marie.martin@gmail.com", "Password456="),
-            new User("Pierre", "Durand", "pierre.durand@gmail.com", "SecurePass789="),
-            new User("Emma", "Johnson", "emma.j@gmail.com", "UserPass321="),
-            new User("Alex", "Smith", "alex.smith@gmail.com", "12345678Ab="),
-            new User("Sophie", "Taylor", "sophie.t@gmail.com", "PassSophie987="),
-            new User("Thomas", "Anderson", "thomas.a@gmail.com", "MatrixPass123="),
-            new User("Laura", "Williams", "laura.w@gmail.com", "Secure123Pass="),
-            new User("Samuel", "Jones", "sam.j@gmail.com", "JonesPass789="),
-            new User("Eva", "Brown", "eva.b@gmail.com", "BrownEva456="),
+            new User(IncrementId(), "Nicolas", "D'Addabbo", "nico.d@gmail.com", "Test123123="),
+            new User(IncrementId(),"Jean", "Dupont", "jean.dupont@gmail.com", "MotDePasse123="),
+            new User(IncrementId(),"Marie", "Martin", "marie.martin@gmail.com", "Password456="),
+            new User(IncrementId(),"Pierre", "Durand", "pierre.durand@gmail.com", "SecurePass789="),
+            new User(IncrementId(),"Emma", "Johnson", "emma.j@gmail.com", "UserPass321="),
+            new User(IncrementId(),"Alex", "Smith", "alex.smith@gmail.com", "12345678Ab="),
+            new User(IncrementId(),"Sophie", "Taylor", "sophie.t@gmail.com", "PassSophie987="),
+            new User(IncrementId(),"Thomas", "Anderson", "thomas.a@gmail.com", "MatrixPass123="),
+            new User(IncrementId(), "Laura", "Williams", "laura.w@gmail.com", "Secure123Pass="),
+            new User(IncrementId(), "Samuel", "Jones", "sam.j@gmail.com", "JonesPass789="),
+            new User(IncrementId(), "Eva", "Brown", "eva.b@gmail.com", "BrownEva456="),
         };
+
+        private static int IncrementId()
+        {
+            return id++;
+        }
 
     }
 }
--- User_API/Models/User.cs
+++ User_API/Models/User.cs
@@ -2,33 +2,38 @@
 {
     public class User
     {
-        #nullable disable
+#nullable disable
         private int _id;
         private string _firstName;
         private string _lastName;
         private string _email;
         private string _password;
         private string _pseudo;
-        public string FirstName { get => _firstName; set => _firstName = value; }
-        public string LastName { get => _lastName; set => _lastName = value; }
-        public string Email { get => _email; set => _email = value; }
-        public string Password { get => _password; set => _password = value; }
-        public string Pseudo { get => _pseudo; set => _pseudo = (value != null) ? value : _firstName+_lastName; }
 
-        public User(string firstName, string lastName, string email, string password)
+
+        public User(int id,string firstName, string lastName, string email, string password)
         {
-            _id = _id + 1;
-            FirstName = firstName;
-            LastName = lastName;
-            Email = email;
-            Password = password;
+            _id = id;
+            _firstName = firstName;
+            _lastName = lastName;
+            _email = email;
+            _password = password;
+            _pseudo = firstName.Substring(0, 2) + lastName.Substring(0, 2);
          
         }
 
-        public User(string firstName, string lastName, string email, string password, string pseudo) : this(firstName, lastName, email, password)
+        public User(int id, string firstName, string lastName, string email, string password, string pseudo) : this(id, firstName, lastName, email, password)
         {
             _pseudo = pseudo;
         }
 
+        public int Id { get => _id;}
+        public string FirstName { get => _firstName; }
+        public string LastName { get => _lastName;  }
+        public string Email { get => _email;  }
+        public string Password { get => _password; }
+        public string Pseudo { get => _pseudo;}
+
     }
+
 }
